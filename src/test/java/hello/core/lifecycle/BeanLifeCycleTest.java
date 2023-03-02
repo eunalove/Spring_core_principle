9781328f05eda16b53c265e057c3a88e8063cfc1
eunalove <88129325+eunalove@users.noreply.github.com>
2023-03-02 22:29:22 +0900

    빈 생명주기 콜백

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycelConfig.class);
+        NetworClient client = ac.getBean(NetworClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycelConfig{
+
+        @Bean
+        public NetworClient networClient(){
+            NetworClient networClient = new NetworClient();
+            networClient.setUrl("http://hello-spring.dev");
+            return networClient;
+        }
+    }
+}
